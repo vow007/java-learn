8a1b134d89f4c25266305e2bef9d41a4e58891fb
zhujin <zhujin111@foxmail.com>
2021-07-09 18:15:24 +0800

    LRU

--- src/algorithm/classic/LRU.java
+++ src/algorithm/classic/LRU.java
@@ -3,18 +3,18 @@ package algorithm.classic;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * https://blog.csdn.net/elricboa/article/details/78847305
- */
 public class LRU<K, V> {
-
-    private static final float hashLoadFactory = 0.75f;
     private LinkedHashMap<K, V> map;
     private int cacheSize;
+    private static final float hashLoadFactory = 0.75f;
 
-    private LRU() {
-    }
-
+    //构造方法
+    /*使用LinkedHashMap实现
+    LinkedHashMap底层就是用的HashMap加双链表实现的，而且本身已经实现了按照访问顺序的存储。
+    此外，LinkedHashMap中本身就实现了一个方法removeEldestEntry用于判断是否需要移除最不常读取的数，
+    方法默认是直接返回false，不会移除元素，所以需要重写该方法。即当缓存满后就移除最不常用的数。
+    原文链接：https://blog.csdn.net/elricboa/article/details/78847305
+    */
     public LRU(int cacheSize) {
         this.cacheSize = cacheSize;
         int capacity = (int) Math.ceil(cacheSize / hashLoadFactory) + 1;
